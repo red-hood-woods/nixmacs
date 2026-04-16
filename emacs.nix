2faeda7a09da3f3a0e8dc8ec7654f6b5d93bdcdd
Alice Liddel <sheepos@proton.me>
2026-04-16 11:52:56 +0000

    mic

---
+++ emacs.nix
@@ -0,0 +1,185 @@
+{ emacs-overlay }:
+
+{ config, pkgs, lib, ... }:
+
+{
+  nixpkgs.overlays = [
+    emacs-overlay.overlay
+  ];
+
+  home.packages = with pkgs; [
+    clang-tools # C/C++ LSP (clangd)
+    pyright     # Python LSP
+    typescript-language-server # JS/TS LSP
+    nixd        # Nix LSP
+  ];
+
+  programs.emacs = {
+    enable = true;
+    package = pkgs.emacs-pgtk; 
+
+    extraPackages = epkgs: with epkgs; [
+      # Evil mode (Vim bindings)
+      evil
+      evil-collection
+      
+      # UI & Theming
+      doom-themes
+      doom-modeline
+      all-the-icons # Note: run 'M-x all-the-icons-install-fonts' once in Emacs
+      dashboard
+      
+      # Completion & Navigation (Modern Helm/Ivy alternative)
+      vertico
+      orderless
+      marginalia
+      consult
+      corfu # In-buffer completion
+      
+      # Key discovery (like Spacemacs)
+      which-key
+      general
+      
+      # Project management & Git
+      projectile
+      magit
+      
+      # Syntax, LSP, and Language Support
+      flycheck
+      lsp-mode
+      lsp-ui
+      company
+      treesit-grammars.with-all-grammars
+      haskell-mode
+      lsp-haskell
+      nix-mode
+      js2-mode
+      typescript-mode
+    ];
+
+    extraConfig = ''
+      ;; --- Spacemacs-like Declarative Config ---
+
+      ;; --- Basic UI ---
+      (setq inhibit-startup-message t)
+      (scroll-bar-mode -1)        ; Disable visible scrollbar
+      (tool-bar-mode -1)          ; Disable the toolbar
+      (tooltip-mode -1)           ; Disable tooltips
+      (set-fringe-mode 10)        ; Give some breathing room
+      (menu-bar-mode -1)          ; Disable the menu bar
+
+      ;; Line numbers
+      (setq display-line-numbers-type 'relative)
+      (global-display-line-numbers-mode t)
+
+      ;; Theme
+      (load-theme 'doom-one t)
+
+      ;; Modeline
+      (doom-modeline-mode 1)
+      (setq doom-modeline-height 35)
+
+      ;; Dashboard
+      (require 'dashboard)
+      (dashboard-setup-startup-hook)
+      (setq dashboard-center-content t)
+      (setq dashboard-items '((recents  . 5)
+                              (projects . 5)))
+
+      ;; --- Evil Mode (Vim bindings) ---
+      (setq evil-want-integration t)
+      (setq evil-want-keybinding nil)
+      (setq evil-want-C-u-scroll t)
+      (setq evil-want-C-i-jump t)
+      (require 'evil)
+      (evil-mode 1)
+
+      (require 'evil-collection)
+      (evil-collection-init)
+
+      ;; --- Which-Key ---
+      (require 'which-key)
+      (which-key-mode)
+      (setq which-key-idle-delay 0.3)
+
+      ;; --- Vertico & Consult (Navigation & Search) ---
+      (require 'vertico)
+      (vertico-mode)
+      
+      (require 'marginalia)
+      (marginalia-mode)
+
+      (require 'orderless)
+      (setq completion-styles '(orderless basic)
+            completion-category-defaults nil
+            completion-category-overrides '((file (styles partial-completion))))
+
+      ;; --- Projectile ---
+      (require 'projectile)
+      (projectile-mode +1)
+
+      ;; --- Keybindings (General.el) ---
+      (require 'general)
+      (general-evil-setup t)
+      
+      (general-create-definer my-leader-def
+        :states '(normal visual insert emacs)
+        :keymaps 'override
+        :prefix "SPC"
+        :global-prefix "C-SPC")
+
+      ;; The Spacemacs "SPC" bindings
+      (my-leader-def
+        "SPC" '(execute-extended-command :which-key "M-x")
+        
+        ;; Files
+        "f"   '(:ignore t :which-key "files")
+        "ff"  '(find-file :which-key "find file")
+        "fs"  '(save-buffer :which-key "save file")
+        "fr"  '(consult-recent-file :which-key "recent files")
+        
+        ;; Buffers
+        "b"   '(:ignore t :which-key "buffers")
+        "bb"  '(consult-buffer :which-key "switch buffer")
+        "bd"  '(kill-current-buffer :which-key "kill buffer")
+        
+        ;; Windows
+        "w"   '(:ignore t :which-key "windows")
+        "wl"  '(evil-window-right :which-key "right")
+        "wh"  '(evil-window-left :which-key "left")
+        "wj"  '(evil-window-down :which-key "down")
+        "wk"  '(evil-window-up :which-key "up")
+        "wv"  '(evil-window-vsplit :which-key "vsplit")
+        "ws"  '(evil-window-split :which-key "split")
+        "wd"  '(evil-window-delete :which-key "delete")
+        
+        ;; Projects
+        "p"   '(:ignore t :which-key "projects")
+        "pf"  '(projectile-find-file :which-key "find file in project")
+        "pp"  '(projectile-switch-project :which-key "switch project")
+        
+        ;; Git
+        "g"   '(:ignore t :which-key "git")
+        "gs"  '(magit-status :which-key "magit status")
+      )
+
+      ;; --- Language Support (Haskell, Nix, C, Python, JS) ---
+      (require 'haskell-mode)
+      (add-hook 'haskell-mode-hook 'lsp)
+      (add-hook 'haskell-literate-mode-hook 'lsp)
+
+      (require 'nix-mode)
+      (add-hook 'nix-mode-hook 'lsp)
+
+      (add-hook 'c-mode-hook 'lsp)
+      (add-hook 'c++-mode-hook 'lsp)
+      
+      (add-hook 'python-mode-hook 'lsp)
+
+      (require 'js2-mode)
+      (add-hook 'js2-mode-hook 'lsp)
+      (require 'typescript-mode)
+      (add-hook 'typescript-mode-hook 'lsp)
+    '';
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "A declarative, Spacemacs-like Emacs configuration flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
+  };
+
+  outputs = { self, nixpkgs, emacs-overlay }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in {
+      # The Home Manager module (for users who want to import it naturally)
+      homeManagerModules.default = import ./emacs.nix { inherit emacs-overlay; };
+
+      # Standalone Emacs packages (for nix run, nix-shell, or macOS/non-NixOS devices!)
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ emacs-overlay.overlay ];
+          };
+
+          # Evaluate the HM module passively to extract configurations
+          mod = (import ./emacs.nix { inherit emacs-overlay; }) { config = {}; pkgs = pkgs; lib = pkgs.lib; };
+          
+          # Extract user configuration
+          emacsConfig = mod.programs.emacs.extraConfig;
+          emacsPackages = mod.programs.emacs.extraPackages;
+          lspPackages = mod.home.packages;
+
+          # For macOS, use standard emacs. For Linux, use pgtk (or base emacs).
+          baseEmacs = if pkgs.stdenv.isDarwin then pkgs.emacs else pkgs.emacs-pgtk;
+
+          # Auto-load our extraConfig by dropping it into site-start.el in the site-lisp directory
+          configPkg = pkgs.runCommand "nixmacs-config" {} ''
+            mkdir -p $out/share/emacs/site-lisp
+            cp ${pkgs.writeText "site-start.el" emacsConfig} $out/share/emacs/site-lisp/site-start.el
+          '';
+
+          # Build Emacs with our extraPackages + configPkg
+          allEmacsPkgs = epkgs: (emacsPackages epkgs) ++ [ configPkg ];
+          emacsWithConfig = (pkgs.emacsPackagesFor baseEmacs).emacsWithPackages allEmacsPkgs;
+
+        in {
+          default = pkgs.symlinkJoin {
+            name = "nixmacs";
+            paths = [ emacsWithConfig ];
+            buildInputs = [ pkgs.makeWrapper ];
+            postBuild = ''
+              # Wrap the emacs binary so it has access to the LSP executables
+              wrapProgram $out/bin/emacs \
+                --prefix PATH : ${pkgs.lib.makeBinPath lspPackages}
+            '';
+          };
+        }
+      );
+    };
+}
